623b452ba8d9444cbafe7c0794ada3078709d1a2
Paijas <matheuspaigel2016@gmail.com>
2024-08-27 16:58:29 -0300

    New: Criando rotas

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 import express from "express"
+import postRoute from "./routes/post.route.js"
+import authRoute from "./routes/auth.route.js"
+
+
 
 const app = express()
+app.use(express.json())
+app.use("/api/post", postRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(3000,()=>{
     console.log("Server está rodando na porta 3000")
